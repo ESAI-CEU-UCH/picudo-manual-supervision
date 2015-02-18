58ef31897d61f0ccb17024d748ff70f0ff777f22
Paco Zamora Martinez <pakozm@gmail.com>
2015-02-18 19:50:39 +0100

    Added shellscript with summary of actions

---
+++ preprocess.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# python scripts/ANNOTATE/generate_wav_mat.py lists/eating_upv.wavs.txt lists/eating_upv.wav_mats.txt
+april-ann scripts/PREPROCESS/preprocess.lua lists/eating_upv.wav_mats.txt lists/eating_upv.fb_mats.txt
